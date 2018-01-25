8db1cda3f5324f8f48100b1b1a9c655ec6e8755d
Hunter <tehunter.2017@mymail.jcsu.edu>
2018-01-25 13:31:19 -0500

    first commit

---
+++ Actor.java
@@ -0,0 +1,22 @@
+
+public class Actor {
+String firstname;
+String lastname;
+	public Actor(String first, String last) {
+		// TODO Auto-generated constructor stub
+	
+		firstname = first;
+		lastname = last;
+		
+Actor actor = new Actor("Arnold","Schwarzenegger");
+System.out.println(actor.firstname);
+System.out.println(actor.lastname);
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
